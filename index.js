b2f3a0fe7fd4a7dd028d8c73c0a228c6000755e5
Ronald <ronaldaarnoutse@gmail.com>
2023-02-22 01:42:37 +0100

    create json with scraped data

--- index.js
+++ index.js
@@ -7,19 +7,60 @@ const PORT = 8000;
 const URL = `https://www.imdb.com/name/${process.env.IMDB_PERSON_ID}/fullcredits`;
 
 const app = express();
+console.clear();
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+
+let $html;
 
 axios(URL).then((response) => {
   const html = response.data;
-  const $ = cheerio.load(html);
-  const jobTitlesFilm = getJobTitlesFilm($);
+  $html = cheerio.load(html, { ignoreWhitespace: true });
+  const jobCategories = getJobCategories();
+  const jobData = getJobData(jobCategories);
+  console.log(JSON.stringify(jobData));
 });
 
-const getJobTitlesFilm = ($) => {
-  const jobTitles = [];
-  $("div.head").each((index, element) => {
-    const id = element.attribs["id"];
-    jobTitles.push(id.substring(11));
+const getJobCategories = () => {
+  const categories = [];
+  $html("div.head").each((index, element) => {
+    categories.push(element.attribs["data-category"]);
   });
-  console.log(jobTitles);
+  return categories;
+};
+
+const getJobData = (jobCategories) => {
+  return jobCategories.map((id) => {
+    const mainSelector = `div.head[data-category='${id}']`;
+    const categoryTitle = $html(`${mainSelector} a`).text();
+    const categoryContent = getCategoryContent(mainSelector);
+    return {
+      id,
+      categoryTitle,
+      categoryContent,
+    };
+  });
+};
+
+const getCategoryContent = (mainSelector) => {
+  const items = [];
+  $html(`${mainSelector} + div .filmo-row`).each((index, element) => {
+    const id = element.attribs["id"];
+    const filmTitle = $html(`#${id} b a`).html();
+    const isSeries = $html(`#${id}`).text().includes("TV Series");
+    const isTvMiniSerie = $html(`#${id}`).text().includes("TV Mini Series");
+    const isTvMovie = $html(`#${id}`).text().includes("TV Movie");
+    const isShort = $html(`#${id}`).text().includes("Short");
+    const year = $html(`#${id} .year_column`).text().replace(/\n/g, "").trim();
+
+    items.push({
+      id,
+      filmTitle,
+      isSeries,
+      isTvMiniSerie,
+      isTvMovie,
+      isShort,
+      year,
+    });
+  });
+  return items;
 };
